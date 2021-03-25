63bf69844e6795d013ecb25ed1ca44b21d5cb90c
BeatriceDuncan <bealmduncan@gmail.com>
2021-03-25 19:14:36 -0400

    added favicon and stretch goal notes to script file

---
+++ script.js
@@ -0,0 +1,17 @@
+// I didn't get to my stretch goals yet but these are my next steps:
+
+// ========= BANNER ========= //
+// Put background pattern in a carousel but have the name & tagline remain fixed with a higher z-index.
+// +++ STRETCH GOAL DELUX: Have a click & drag tab at the bottom of the banner, when user drags the tab the background pattern changes but the name & tagline remain fixed with a higher z-index. 
+
+// ========= NAV ========= //
+// Make ^ in Nav only appear when header is out of view.
+// Hamburger Nav.
+
+// ========= WORKS IN PROGRESS ========= //
+// Display these projects in a carousel.
+// Display images only and show project info on hover.
+
+// ========= CONTACT ========= //
+// Create another carousel or pop-up-book style transition into another collage of me but as a small child.
+// Once my domain has transferred over from Wix to Hover, I will set up a contact form linked to hello@beatriceduncan.com.
